2c4a1cf12993abb9515961e158976efc0a6d1a22
Kvcurek <81987976+Kvcurek@users.noreply.github.com>
2023-04-16 12:06:08 +0200

    first test commit

--- colors.ts
+++ colors.ts
@@ -1,1 +1,14 @@
+const availableColors = {
+    red: ""
+}
 
+const availableFormats = {
+    reset: ""
+}
+
+export class Colors {
+    public red (text: String) {
+        if (!text) return;
+        return `${availableColors.red}${text}${availableFormats.reset}`;
+    }
+}
